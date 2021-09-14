311b648183b9ca763142fb0d78e2fbb21f4071a1
ramroomh <rhamati95@gmail.com>
2021-09-14 12:31:31 -0400

    rearranges columns for BIDS

--- csv2tab.py
+++ csv2tab.py
@@ -4,6 +4,8 @@ import pandas as pd
 # Define input
 in_file = sys.argv[1]
 # Read in csv
-df = pd.read_csv(in_file, header=None)
+df = pd.read_csv(in_file, names=['onset','value','duration','weight'], sep=' ')
+# Rearrange
+df = df[['onset','duration','value','weight']]
 # Add header, add tabs
-df.to_csv(in_file, header=["onset","value","duration","weight"], sep="\t", index=False)
+df.to_csv(in_file, header=True, sep="\t", index=False)
