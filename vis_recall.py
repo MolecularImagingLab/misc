e794ed5fa2f29b087b87a5d6b8b10b4ed74329ba
ramroomh <rhamati95@gmail.com>
2022-11-15 00:04:02 -0500

    cleaned and updated py3.8.5

--- vis_recall.py
+++ vis_recall.py
@@ -2,68 +2,82 @@
 import os, sys, glob
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
-from matplotlib.ticker import FixedLocator
-import seaborn as sns
 
-# Grab subjects
+def grab_subs(path):
+    subs_paths = os.listdir(path)
+    subs = sorted([p for p in subs_paths if 'AVL' in p])
+    return subs
+
+def grab_parord(subs):
+    par_order = {}
+    for s in subs:
+        grab_par = glob.glob(os.path.join(path, s,'*csv'))
+        filt_rec = [p for p in grab_par if 'RECALL' not in p]
+        oresults = sorted (filt_rec, key = lambda filt_rec: int(filt_rec[-12:-4]))
+        par_order[s] = [o[-25:-24] for o in oresults]
+    return par_order
+
+def collect_recalls(path, subs, par_order):
+    recalls = []
+    recode = {'very_unlikely':1,'somewhat_unlikely':2,'somewhat_likely':3,'very_likely':4}
+    # Load recall files and append results
+    for s in subs:
+        grab_par = glob.glob(os.path.join(path, s,'*csv'))
+        # Filter for recall files
+        filt_rec = [p for p in grab_par if 'RECALL' in p]
+        # Sort recall by time
+        recall_ord = sorted (filt_rec, key = lambda filt_rec: int(filt_rec[-12:-4]))
+        # Add data to master list
+        for recall in recall_ord:
+            m = pd.read_csv(recall)
+            n = pd.DataFrame.replace(m,to_replace=recode)
+            # Get ID, session == index
+            n['ID'] = s
+            n['Session'] = str(recall_ord.index(recall) +1)
+            # Get status
+            if int(s[-3:]) < 100 or 199 < int(s[-3:]) < 299:
+                n['Status'] = 'control'
+            else:
+                n['Status'] = 'FDR'
+            # Get run == index
+            for order in par_order[s]:
+                x = str(par_order[s].index(order) +1)
+                n.loc[n['Stimulus'].str.startswith(order), 'Run'] = x
+            recalls.append(n)
+    # Concat all subjects
+    recall_vals = pd.concat([rec for rec in recalls], ignore_index=True)
+    recall_vals.fillna('Sham', inplace=True)
+    # Sort by letter
+    for letter in ['A','B','C','D','E']:
+        recall_vals.loc[recall_vals['Stimulus'].str.startswith(letter), 'Run Type'] = letter
+    # Aggregate csp and csm events
+    recall_vals.loc[recall_vals['Stimulus'].str.endswith('CSm'), 'Stimulus'] = 'CSm'
+    recall_vals.loc[recall_vals['Stimulus'].str.endswith('CSp'), 'Stimulus'] = 'CSp'
+    return recall_vals
+
+def learning_idxs(df):
+    agg = df.groupby(['ID','Stimulus','Run', 'Run Type'], as_index=False).mean()
+    diffs = []
+    for s in df['ID'].unique():
+        for r in ['1', '2', '3', '4', 'Sham']:
+            try:
+                csp = float(agg.loc[(agg['ID'] == s) & (agg['Stimulus'] == 'CSp') & (agg['Run'] == r), 'Response'].values)
+                csm = float(agg.loc[(agg['ID'] == s) & (agg['Stimulus'] == 'CSm') & (agg['Run'] == r), 'Response'].values)
+                diff = csp - csm
+                di = (csp - csm)/(csp + csm)
+                runtype = (agg.loc[(agg['ID'] == s) & (agg['Run'] == r), 'Run Type'].values)[0]
+                diffs.append([s, r, diff, di, runtype])
+            except:
+                pass
+    learning = pd.DataFrame(diffs, columns=['ID', 'Run', 'Contrast', 'DI', 'Run Type'])
+    return learning
+
 path = '/Users/ramihamati/Documents/PhD_Work/AVL/PAR_FILES'
-subs_paths = glob.glob(os.path.join(path, 'AVL*'))
-subs = [s[-7:] if len(s) == 58 else s[-10:] for s in subs_paths]
-
-# Grab run order
-par_order = {}
-for s in subs:
-    grab_par = glob.glob(os.path.join(path, s,'*csv'))
-    filt_rec = [p for p in grab_par if 'RECALL' not in p]
-    oresults = sorted (filt_rec, key = lambda filt_rec: int(filt_rec[-12:-4]))
-    par_order[s] = [o[-25:-24] for o in oresults]
-
-# Create an empty dataframe
-data = pd.DataFrame(columns=['Stimulus','Response','RT','ID','Session','Run'])
-recode = {'very_unlikely':1,'somewhat_unlikely':2,'somewhat_likely':3,'very_likely':4}
-# Load recall files and append results
-for s in subs:
-    grab_par = glob.glob(os.path.join(path, s,'*csv'))
-    # Filter for recall files
-    filt_rec = [p for p in grab_par if 'RECALL' in p]
-    # Sort recall by time
-    recall_ord = sorted (filt_rec, key = lambda filt_rec: int(filt_rec[-12:-4]))
-    # Add data to master list
-    for recall in recall_ord:
-        m = pd.read_csv(recall)
-        n = pd.DataFrame.replace(m,to_replace=recode)
-        # Get ID, session == index
-        n['ID'] = s
-        n['Session'] = str(recall_ord.index(recall) +1)
-        # Group by Stimulus type, according to ID and session
-        #n = n.groupby(['ID','Session','Stimulus'], as_index=False).mean()
-        # Get status
-        if int(s[-3:]) < 100 or 199 < int(s[-3:]) < 299:
-            n['Status'] = 'control'
-        else:
-            n['Status'] = 'FDR'
-        # Get run == index
-        for order in par_order[s]:
-            x = str(par_order[s].index(order) +1)
-            n.loc[n['Stimulus'].str.startswith(order), 'Run'] = x
-        data = data.append(n, ignore_index=True,sort=False)
-data.fillna('Sham', inplace=True)
-
-# Aggregate csp and csm events
-data.loc[data['Stimulus'].str.endswith('m'), 'Stimulus'] = 'CSm'
-data.loc[data['Stimulus'].str.endswith('p'), 'Stimulus'] = 'CSp'
+subs = grab_subs(path)
+par_order = grab_parord(subs)
+recall_vals = collect_recalls(path, subs, par_order)
 # Filter data for healthy controls
-hc = data.loc[(data['Status'] == 'control') & (data['Session'] == '1')]
-
+hc = recall_vals.loc[(recall_vals.Status == 'control') & (recall_vals.Session == '1')]
 # Get learning indices
-agg = hc.groupby(['ID','Stimulus','Run'], as_index=False).mean()
-diffs = []
-for s in agg['ID'].unique():
-    for r in ['1', '2', '3', '4', 'Sham']:
-        csp = float(agg.loc[(agg['ID'] == s) & (agg['Stimulus'] == 'CSp') & (agg['Run'] == r), 'Response'].values)
-        csm = float(agg.loc[(agg['ID'] == s) & (agg['Stimulus'] == 'CSm') & (agg['Run'] == r), 'Response'].values)
-        diff = csp - csm
-        di = (csp - csm)/(csp + csm)
-        diffs.append([s, r, diff, di])
-learning = pd.DataFrame(diffs, columns=['ID', 'Run', 'Contrast', 'DI'])
+learning = learning_idxs(hc)
+learning.to_csv('/Users/ramihamati/Downloads/learning.csv')
